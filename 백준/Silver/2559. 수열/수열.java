328d3a593511f9e1546e4eacc4633fe3d62e085d
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2022-11-20 22:48:28 +0900

    [Silver III] Title: 수열, Time: 1092 ms, Memory: 24112 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,7 +1,3 @@
-
-/*
- * 수열 - 구현
- */
 import java.util.*;
 import java.io.*;
 
@@ -14,12 +10,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken()); 
 		arr = new int[n];
-		int start = 0;
-		int end = n-1;
-		int r_sum = 0;
-		int l_sum = 0;
+		int sum = 0;
 		int result = Integer.MIN_VALUE;
 		st = new StringTokenizer(br.readLine());
 		
@@ -27,25 +20,16 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i=0;i<n;i++) {
-			r_sum = 0;
-			l_sum = 0;
-			for(int j =start;j<=start+k-1;j++) {
-				r_sum+=arr[j];
+		for(int i=0;i<=n-k;i++) {
+			sum = 0;
+			for(int j=i;j<i+k;j++) {
+				sum+=arr[j];
 			}
-			
-			for(int j=end;j>=end-k+1;j--) {
-				l_sum+=arr[j];
-			}
-		
-			result = Math.max(result, Math.max(r_sum, l_sum));
-			if(start+k==n)break;
-			if(end-k==0)break;
-			start++;
-			end--;
+			result = Math.max(result, sum);
 		}
 		System.out.println(result);
+		
+
 	}
 
 }
-
