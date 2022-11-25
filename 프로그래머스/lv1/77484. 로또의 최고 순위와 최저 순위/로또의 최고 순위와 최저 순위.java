387f191abba0de1119fa48fe6bdcde0ca95512a2
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2022-11-25 21:13:49 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int hit = 0;
+        int max = 0;
+        
+        for(int i=0;i<lottos.length;i++){
+            boolean check = false;
+            for(int j=0;j<win_nums.length;j++){
+                if(lottos[i]==win_nums[j]){
+                    check=true;
+                    hit++;
+                }
+            }
+            if(check==false){
+                if(lottos[i]!=0)lottos[i]=-1;
+            }
+        }
+        
+        for(int i=0;i<lottos.length;i++){
+            if(lottos[i]!=-1)max++;
+        }
+        
+        if(max==6)answer[0]=1;
+        else if(max==5)answer[0]=2;
+        else if(max==4)answer[0]=3;
+        else if(max==3)answer[0]=4;
+        else if(max==2)answer[0]=5;
+        else answer[0]=6;
+        
+        if(hit==6)answer[1]=1;
+        else if(hit==5)answer[1]=2;
+        else if(hit==4)answer[1]=3;
+        else if(hit==3)answer[1]=4;
+        else if(hit==2)answer[1]=5;
+        else answer[1]=6;
+
+        return answer;
+    }
+}
