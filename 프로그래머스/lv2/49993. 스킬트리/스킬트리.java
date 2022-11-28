c7f1a5d833ddf6b100dd82de95a2749db3aa1f71
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2022-11-28 18:58:39 +0900

    [level 2] Title: 스킬트리, Time: 10.30 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -2,39 +2,47 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = -1;
-        int count = 0;
-        Queue<Character> q = new LinkedList<>();
+//         int count = 0;
+//         Queue<Character> q = new LinkedList<>();
         
-        StringBuilder sb;
-        for(int i=0;i<skill_trees.length;i++){
-            boolean check = false;
-            q.clear();
-            sb = new StringBuilder();
-            char[] arr = skill_trees[i].toCharArray();
+//         StringBuilder sb;
+//         for(int i=0;i<skill_trees.length;i++){
+//             boolean check = false;
+//             q.clear();
+//             sb = new StringBuilder();
+//             char[] arr = skill_trees[i].toCharArray();
             
-            for(int j=0;j<arr.length;j++){
-                if(skill.contains(Character.toString(arr[j]))){
-                    check=true;
-                    if(q.contains(arr[j]))continue;
-                    else q.offer(arr[j]);
-                }
-            }
-            if(check==false){
-                count++;
-            }
-            while(!q.isEmpty()){
-                sb.append(q.poll());
-            }
+//             for(int j=0;j<arr.length;j++){
+//                 if(skill.contains(Character.toString(arr[j]))){
+//                     check=true;
+//                     if(q.contains(arr[j]))continue;
+//                     else q.offer(arr[j]);
+//                 }
+//             }
+//             if(check==false){
+//                 count++;
+//             }
+//             while(!q.isEmpty()){
+//                 sb.append(q.poll());
+//             }
          
-            String s = sb.toString();
-            if(s.startsWith(Character.toString(skill.charAt(0)))){
-                if(skill.contains(s)){
-                    count++;
-                }
-            }
+//             String s = sb.toString();
+//             if(s.startsWith(Character.toString(skill.charAt(0)))){
+//                 if(skill.contains(s)){
+//                     count++;
+//                 }
+//             }
             
+//         }
+        
+        ArrayList<String> skilltrees = new ArrayList<>(Arrays.asList(skill_trees));
+        Iterator<String> it = skilltrees.iterator();
+        while(it.hasNext()){
+            if(skill.indexOf(it.next().replaceAll("[^"+skill+"]",""))!=0){
+                it.remove();
+            }
         }
-        answer = count;
+        answer = skilltrees.size();
         return answer;
     }
 }
