359d7d6cd02c6c54b30a6b6ec87ceb40d034db66
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-02-08 17:42:03 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.20 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int dp[];
+    public int solution(int n) {
+        int answer = 0;
+        dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        
+        if(n>=2){
+            dp[2]=2;
+            for(int i=3;i<=n;i++){
+                dp[i]=(dp[i-2]+dp[i-1])%1000000007;
+            }
+        }
+        answer = dp[n]%1000000007;
+        return answer;
+    }
+}
