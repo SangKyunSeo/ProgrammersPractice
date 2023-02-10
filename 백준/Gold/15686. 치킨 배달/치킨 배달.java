8ab4f20804d354bf56935f1b8bc4218cce70f283
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-02-10 15:59:00 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 16440 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,24 +2,22 @@
 /*
  * 치킨 배달
  */
-import java.io.*;
-import java.util.*;
 
-class point{
-	int x,y;
-	point(int x,int y){
-		this.x = x;
-		this.y = y;
-	}
-}
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
 	static int n,m;
-	static int[][] map;
-	static int min = Integer.MAX_VALUE;
-	static List<point> chicken = new ArrayList<>();
-	static List<point> home = new ArrayList<>();
+	static int map[][];
+	static int chickens[][];
+	static int count = 0;
+	static int homeCount = 0;
 	static boolean visit[];
+	static int result = 0;
+	static int resultMin = Integer.MAX_VALUE;
+	static int home[][];
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,42 +29,59 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<n;j++) {
 				map[i][j]=Integer.parseInt(st.nextToken());
+				if(map[i][j]==1)homeCount++;
+				if(map[i][j]==2)count++;
 			}
 		}
+		visit = new boolean[count];
+		chickens = new int[count][2];
+		home = new int[homeCount][2];
 		
+		find();
+		
+		dfs(0,0);
+		System.out.println(resultMin);
+	}
+	public static void find() {
+		int idx = 0;
+		int hIdx = 0;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				if(map[i][j]==1)home.add(new point(i,j));
-				else if(map[i][j]==2)chicken.add(new point(i,j));
+				if(map[i][j]==1) {
+					home[hIdx][0]=i;
+					home[hIdx][1]=j;
+					hIdx++;
+				}
+				if(map[i][j]==2) {
+					chickens[idx][0]=i;
+					chickens[idx][1]=j;
+					idx++;
+				}
 			}
 		}
-		visit = new boolean[chicken.size()];
-		dfs(0,0);
-		System.out.println(min);
-
 	}
-	public static void dfs(int start,int open) {
-		if(open==m) {
-			int count = 0;
-			for(int i=0;i<home.size();i++) {
-				int temp = Integer.MAX_VALUE;
-				for(int j=0;j<chicken.size();j++) {
+	public static void dfs(int start,int depth) {
+		if(depth==m) {
+			result = 0;
+			for(int i=0;i<home.length;i++) {
+				int min = Integer.MAX_VALUE;
+				for(int j=0;j<visit.length;j++) {
 					if(visit[j]) {
-						int distance = Math.abs(chicken.get(j).x-home.get(i).x)+Math.abs(chicken.get(j).y-home.get(i).y);
-						temp = Math.min(temp, distance);
+						min = Math.min(min, Math.abs(home[i][0] - chickens[j][0]) + Math.abs(home[i][1] - chickens[j][1]));
 					}
 				}
-				count+=temp;
-				
+				result+=min;
 			}
-			min = Math.min(min, count);
+			resultMin = Math.min(result, resultMin);
 			return;
 		}
 		
-		for(int i=start;i<chicken.size();i++) {
-			visit[i]=true;
-			dfs(i+1,open+1);
-			visit[i]=false;
+		for(int i=start;i<count;i++) {
+			if(!visit[i]) {
+				visit[i]=true;
+				dfs(i+1,depth+1);
+				visit[i]=false;
+			}
 		}
 	}
 
