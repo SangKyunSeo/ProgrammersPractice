2d1bea3878b55a2ae1f14fbb26c795a1f634bb7c
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-02-23 15:08:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1352 ms, Memory: 75876 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,42 +1,45 @@
 
 /*
- * tree의 부모 찾기
+ * 트리의 부모 찾기
  */
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
+
+	static int n;
 	static List<Integer> list[];
-	static int[] parents;
-	
+	static int[] parent;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
 		list = new ArrayList[n+1];
-		parents = new int[n+1];
+		parent = new int[n+1];
 		
-		for(int i =0;i<n+1;i++) {
-			list[i] = new ArrayList<>();
+		for(int i=0;i<n+1;i++) {
+			list[i] = new ArrayList<>(); 
 		}
 		
-		for(int i =0;i<n-1;i++) {
-			 st = new StringTokenizer(br.readLine());
-			 int a = Integer.parseInt(st.nextToken());
-			 int b = Integer.parseInt(st.nextToken());
-			 list[a].add(b);
-			 list[b].add(a);
+		for(int i=0;i<n-1;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
 		}
 		
-		dfs(list,parents,1,0);
+		dfs(1);
+		for(int i=2;i<parent.length;i++) {
+			System.out.println(parent[i]);
+		}
 		
-		for(int j=2;j<n+1;j++)System.out.println(parents[j]);
 	}
-	public static void dfs(List<Integer> list[], int[] parents,int start,int parent) {
-		parents[start]=parent;
-		for(int i:list[start]) {
-			if(i!=parent)dfs(list,parents,i,start);
+	public static void dfs(int p) {
+		for(int i : list[p]) {
+			if(parent[i]==0) {
+				parent[i]=p;
+				dfs(i);
+			}
 		}
 	}
 
