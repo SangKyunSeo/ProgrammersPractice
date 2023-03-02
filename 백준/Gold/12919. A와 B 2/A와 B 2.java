55a6c90ffec5f7494ad2454085151850da6ad399
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-03-02 16:19:03 +0900

    [Gold V] Title: A와 B 2, Time: 208 ms, Memory: 25680 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -9,36 +9,30 @@ public class Main {
 
 	static String s;
 	static String t;
-	static int check = 0;
+	static boolean flag = false;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		s = br.readLine();
 		t = br.readLine();
 		
+	
 		dfs(s);
-		if(check!=0)System.out.println(1);
+		
+		if(flag)System.out.println(1);
 		else System.out.println(0);
 	}
 	public static void dfs(String s) {
-		if(s.length()==t.length()) {
-			if(s.equals(t)) {
-				check=1;
-			}
+		if(s.length()>=t.length()) {
+			if(s.equals(t))flag = true;
 			return;
 		}
-		
-		if(t.contains(reverse(s+"A"))||t.contains(s+"A")) {
+		StringBuilder sb = new StringBuilder(s);
+		StringBuilder ssb = new StringBuilder(s);
+		if(t.contains(s+"A")||t.contains(ssb.append("A").reverse().toString())){
 			dfs(s+"A");
-			dfs(reverse(s+"B"));
+			dfs("B"+sb.reverse().toString());
+		}else {
+			dfs("B"+sb.reverse().toString());
 		}
-		else dfs(reverse(s+"B"));	
 	}
-	public static String reverse(String s) {
-		StringBuilder sb = new StringBuilder();
-		for(int i=s.length()-1;i>=0;i--) {
-			sb.append(s.charAt(i));
-		}
-		return sb.toString();
-	}
-
 }
