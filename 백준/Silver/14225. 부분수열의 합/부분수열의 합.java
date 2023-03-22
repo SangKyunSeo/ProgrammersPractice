36dfaca3d4fa0136f2b364d1a92e4a4835794b09
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-03-22 13:48:27 +0900

    [Silver I] Title: 부분수열의 합, Time: 148 ms, Memory: 22148 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -1,49 +1,44 @@
 
-/*
- * 부분 수열의 합 - 브루트포스
- */
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
 	static int n;
-	static List<Integer> list = new ArrayList<>();
-	static List<Integer> result = new ArrayList<>();
-	static int[] arr = new int[2000001];
-	static int min = Integer.MAX_VALUE;
-	
+	static int arr[];
+	static int array[] = new int[2000001];
+	static boolean visit[];
+	static int result = 0;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		visit = new boolean[n];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		for(int i=0;i<n;i++) {
-			list.add(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		dfs(0,0);
 	
-		Collections.sort(result);
-		for(int i=0;i<result.size();i++) {
-			arr[result.get(i)]=1;
-		}
+		dfs(0,0);
 		
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]==0) {
-				System.out.println(i);
+		for(int i=1;i<array.length;i++) {
+			if(array[i]==0) {
+				result=i;
 				break;
 			}
 		}
+		System.out.println(result);
 	}
-	public static void dfs(int index,int sum) {
-		if(index==n) {
-			result.add(sum);
+	public static void dfs(int depth,int sum) {
+		if(depth==n) {
+			array[sum]=1;
 			return;
 		}
+		dfs(depth+1,sum+arr[depth]);
+		dfs(depth+1,sum);
 		
-		dfs(index+1,sum+list.get(index));
-		dfs(index+1,sum);
 	}
 
 }
