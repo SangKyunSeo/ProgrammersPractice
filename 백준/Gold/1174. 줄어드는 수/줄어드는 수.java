2baac357ddaa0551ce14744c03456bb6fda3ace2
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-04-03 14:02:04 +0900

    [Gold V] Title: 줄어드는 수, Time: 144 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -1,36 +1,48 @@
 
-/*
- * 줄어드는 수
- */
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int num[] = {9,8,7,6,5,4,3,2,1,0};
-	static List<Long> list = new ArrayList<>();
 	static int n;
+	static List<Long> list = new ArrayList<>();
+	static int array[] = {9,8,7,6,5,4,3,2,1,0};
+	static boolean visit[] = new boolean[10];
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		
-		dfs(0,0);
+		dfs(0);
 		Collections.sort(list);
-		if(n>1023) {
+		if(list.size()<n)
 			System.out.println(-1);
-		}else {
+		else
 			System.out.println(list.get(n-1));
-		}
 	}
-	public static void dfs(long sum,int idx) {
-		if(!list.contains(sum))list.add(sum);
-		
-		if(idx>=10) {
+	public static void dfs(int depth) {
+		if(depth==10) {
 			return;
 		}
 		
-		dfs(sum*10 + num[idx],idx+1);
-		dfs(sum,idx+1);
+		for(int i=depth;i<10;i++) {
+			if(!visit[i]) {
+				visit[i]=true;
+				plus();
+				dfs(i+1);
+				visit[i]=false;
+			}
+		}
+	}
+	public static void plus() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<visit.length;i++) {
+			if(visit[i]) {
+				sb.append(array[i]);
+			}
+		}
+		
+		list.add(Long.parseLong(sb.toString()));
 	}
 
 }
