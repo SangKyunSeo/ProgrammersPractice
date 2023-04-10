67810e245f2fdb7bf6547a6644f443d37ef70aa3
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-04-10 21:35:52 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18272 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,7 +1,3 @@
-
-/*
- * 1로만들기 - dp
- */
 import java.util.*;
 import java.io.*;
 
@@ -12,28 +8,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		dp = new int[n+1];
-		if(n==1) dp[1]=0;
-		if(n==2) dp[2]=1;
-		if(n==3) dp[3]=1;
-		if(n>=4) {
-			dp[1]=0;
-			dp[2]=1;
-			dp[3]=1;
-			for(int i=4;i<=n;i++) {
-				if(i%2==0&&i%3==0) {
-					dp[i]=Math.min(dp[i-1], Math.min(dp[i/2], dp[i/3]))+1;
-				}
-				else if(i%2==0) {
-					dp[i]=Math.min(dp[i-1], dp[i/2])+1;
-				}else if(i%3==0) {
-					dp[i]=Math.min(dp[i-1], dp[i/3])+1;
-				}else {
-					dp[i]=dp[i-1]+1;
-				}
-			}
+		dp[0]=0;
+		dp[1]=0;
+		for(int i=2;i<=n;i++) {
+			if(i%6==0)dp[i] = Math.min(dp[i-1],Math.min(dp[i/2], dp[i/3]))+1;
+			else if(i%2==0)dp[i] = Math.min(dp[i-1], dp[i/2])+1;
+			else if(i%3==0)dp[i] = Math.min(dp[i-1], dp[i/3])+1;
+			else dp[i]=dp[i-1]+1;
 		}
-		
 		System.out.println(dp[n]);
 	}
-
+	
 }
