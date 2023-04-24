0c8881886850c33eeff3f76f54add974aeb605d4
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2023-04-24 09:03:58 +0900

    [Silver I] Title: 스티커, Time: 740 ms, Memory: 110936 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,49 +1,36 @@
 
-import java.util.*;
 import java.io.*;
-
+import java.util.StringTokenizer;
 public class Main {
-	static int t,n;
-	static int[][] map;
-	static int[][] dp;
-	static int sum = 0;
-	static int max = Integer.MIN_VALUE;
-	
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		t = Integer.parseInt(br.readLine());
 		
-		for(int test = 0;test<t;test++) {	
-			n = Integer.parseInt(br.readLine());
-			map = new int[2][n];
-			dp = new int[2][n];
-			sum = 0;
-			max = Integer.MIN_VALUE;
-			
-			for(int i=0;i<2;i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0;j<n;j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<t;i++) {
+			int n = Integer.parseInt(br.readLine());
+			int dp[][] = new int[2][n+1];
+			int map[][] = new int[2][n+1];
+			for(int j=0;j<2;j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for(int k=1;k<=n;k++) {
+					map[j][k] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			dp[0][0]=map[0][0];
-			dp[1][0]=map[1][0];
 			
-			if(n>1) {
-				dp[0][1]=dp[1][0]+map[0][1];
-				dp[1][1]=dp[0][0]+map[1][1];
-				for(int i=2;i<n;i++) {
-					dp[0][i] = Math.max(dp[1][i-2], dp[1][i-1]) + map[0][i];
-					dp[1][i] = Math.max(dp[0][i-2], dp[0][i-1]) + map[1][i];
-				}
+			dp[0][1]=map[0][1];
+			dp[1][1]=map[1][1];
+			
+			for(int j=2;j<=n;j++) {
+				dp[0][j] = Math.max(dp[1][j-1], dp[1][j-2]) + map[0][j];
+				dp[1][j] = Math.max(dp[0][j-1], dp[0][j-2]) + map[1][j];
 			}
 			
-			System.out.println(Math.max(dp[0][n-1], dp[1][n-1]));
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 			
 		}
 
 	}
-
 }
