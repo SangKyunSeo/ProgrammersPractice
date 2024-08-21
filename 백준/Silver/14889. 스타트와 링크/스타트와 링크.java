d54926ce342da1cccc3f1d1d943eef1e7089c999
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-08-21 21:25:25 +0900

    [Silver I] Title: 스타트와 링크, Time: 372 ms, Memory: 19804 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,82 +1,67 @@
 
-/*
- * 스타트와 링크 백트래킹, 브루트포스
- */
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[][] map;
+    static boolean[] visit;
+    static int startScore = 0;
+    static int linkScore = 0;
+    static int result = Integer.MAX_VALUE;
 
-	static int n;
-	static int[][] s;
-	static int[] arr;
-	static boolean[] v;
-	static int s_skill = 0;
-	static int l_skill = 0;
-	static int min = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		s = new int[n+1][n+1];
-		arr = new int[n/2];
-		v = new boolean[n+1];
-		
-		// Insert value 
-		for(int i=1;i<=n;i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			for(int j=1;j<=n;j++) {
-				s[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		dfs(1,0);
-		System.out.println(min);
-		
-	}
-	public static void dfs(int at,int depth) {
-		if(depth==n/2) {
-			for(int i=0;i<arr.length;i++) {
-				v[arr[i]]=true;
-			}
-			diff();
-			for(int i=0;i<arr.length;i++) {
-				v[arr[i]]=false;
-			}
-			s_skill = 0;
-			l_skill = 0;
-			return;
-		}
-		
-		for(int i=at;i<=n;i++) {
-			arr[depth]=i;
-			dfs(i+1,depth+1);
-		}
-	}
-	private static void diff() {
-		for(int i=1;i<n;i++) {
-			for(int j=i+1;j<=n;j++) {
-				if(v[i]==true&&v[j]==true) {
-					s_skill+=s[i][j];
-					s_skill+=s[j][i];
-				}else if(v[i]!=true&&v[j]!=true) {
-					l_skill+=s[i][j];
-					l_skill+=s[j][i];
-				}
-			}
-		}
-		
-		int val = Math.abs(l_skill-s_skill);
-		if(val==0) {
-			System.out.println(val);
-			System.exit(0);
-		}
-		
-		min = Math.min(min, val);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visit = new boolean[n];
+
+        for(int i = 0; i < map.length; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < map[0].length; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int end = n / 2;
+
+        sol(0,0, end);
+        System.out.println(result);
+
+    }
+
+    public static void sol(int idx, int depth, int end){
+        if(depth == end){
+            getResult();
+            return;
+        }
+        for(int i = idx; i < map.length; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                sol(i, depth + 1, end);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public static void getResult(){
+        startScore = 0;
+        linkScore = 0;
+        for(int i = 0; i < map.length - 1; i++){
+            for(int j = i+1; j < map[0].length; j++){
+                if(visit[i] && visit[j] && i != j){
+                    startScore += map[i][j];
+                    startScore += map[j][i];
+                }else if(!visit[i] && !visit[j] && i != j){
+                    linkScore += map[i][j];
+                    linkScore += map[j][i];
+                }
+            }
+        }
+
+        result = Math.min(Math.abs(startScore - linkScore), result);
+    }
+
 
 }
