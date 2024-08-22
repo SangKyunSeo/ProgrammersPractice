89f6244e6a35af88d140bcb28a22616eb27a1f3f
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-08-22 23:03:03 +0900

    [Gold V] Title: 리모컨, Time: 236 ms, Memory: 76224 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,111 +1,69 @@
 
-
-/*
- * 리모컨 - 브루트 포스
- */
-
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+    static char notUse[];
+    static int result = Integer.MAX_VALUE;
 
-	static int n;
-	static int k;
-	static List<Character> list = new ArrayList<>();
-	static int countUp = 0;
-	static int countDown = 0;
-	static int onlyUp = 0;
-	static int onlyDown = 0;
-	static int min = Integer.MAX_VALUE;
-	static int onlyMin = Integer.MAX_VALUE;
-	static int a = 0, b=0;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		k = Integer.parseInt(br.readLine());
-		if(k!=0) {
-			st = new StringTokenizer(br.readLine());
-			for(int i=0;i<k;i++) {
-				list.add(st.nextToken().charAt(0));
-			}
-		}
-	
-		if(n==100) {
-			System.out.println(0);
-			return;
-		}
-		
-		checkUp(n);
-		checkDown(n);
-		
-		min = Math.min(countUp + String.valueOf(b).length(), countDown + String.valueOf(a).length());
-		
-		Up(n);
-		Down(n);
-		onlyMin = Math.min(onlyUp, onlyDown);
-		min = Math.min(min, onlyMin);
-		
-		System.out.println(min);
-	}
-	public static void Up(int num) {
-		int start = 100;
-		while(true) {
-			if(onlyUp == 500000) break;
-			if(start!=num) {
-				start++;
-				onlyUp++;
-			}else {
-				break;
-			}
-		}
-	}
-	public static void Down(int num) {
-		int start = 100;
-		while(true) {
-			if(onlyDown == 500000) break;
-			if(start!=num) {
-				start--;
-				onlyDown++;
-			}else {
-				break;
-			}
-		}
-	}
-	public static void checkUp(int num) {
-		while(true) {
-			
-			if(countDown == 500000)break;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-			if(contain(num)) {
-				countDown++;
-				num--;
-			}
-			else {
-				a = num;
-				break;
-			}
-		}
-	}
-	
-	public static void checkDown(int num) {
-		while(true) {
-			
-			if(countUp == 500000)break;
-			if(contain(num)) {
-				countUp++;
-				num++;
-			}else {
-				b = num;
-				break;
-			}
-		}
-	}
-	public static boolean contain(int num) {
-		char arr[] = String.valueOf(num).toCharArray();
-		for(int i=0;i<arr.length;i++) {
-			if(list.contains(arr[i])) return true;
-		}
-		return false;
-	}
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        if(m > 0) {
+            st = new StringTokenizer(br.readLine());
+        }else{
+            result = Math.min(Math.abs(n - 100), Integer.toString(n).length());
+            System.out.println(result);
+            return;
+        }
+
+        notUse = new char[m];
+
+        for(int i = 0; i < notUse.length; i++){
+            notUse[i] = st.nextToken().charAt(0);
+        }
+
+        if(n == 100){
+            System.out.println(0);
+            return;
+        }
+
+        if(m == 10){
+            System.out.println(Math.abs(100 - n));
+            return;
+        }
+        result = Math.min(result, Math.abs(100 - n));
+        sol(n);
+        System.out.println(result);
+    }
+
+    public static void sol(int n){
+        for(int i = 0; i <= 999999; i++){
+            check(i, Math.abs(n - i));
+        }
+    }
+
+    public static void check(int n, int sum){
+        String s = Integer.toString(n);
+        char sArr[] = s.toCharArray();
+
+        for(char c : sArr){
+            for(char nc : notUse){
+                if(c == nc) return;
+            }
+        }
+
+        result = Math.min(result, sum + sArr.length);
+
+    }
+
 }
