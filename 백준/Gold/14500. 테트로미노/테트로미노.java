49be7ff5c552bf9f5cd32ba29a3ed1c9ec8a688a
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-08-24 13:40:38 +0900

    [Gold IV] Title: 테트로미노, Time: 680 ms, Memory: 35896 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,66 +1,70 @@
-import java.util.*;
-import java.io.*;
+
+import com.sun.jdi.IntegerType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n,m;
-	static int[][] map;
-	static boolean[][] visit;
-	static int dx[] = {-1,1,0,0};
-	static int dy[] = {0,0,-1,1};
-	static int max = Integer.MIN_VALUE;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		visit = new boolean[n][m];
-		
-		for(int i=0;i<n;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<m;j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				visit[i][j]=true;
-				dfs(i,j,1,map[i][j]);
-				visit[i][j]=false;
-			}
-		}
-		
-		System.out.println(max);
+    static int[][] map;
+    static int result = Integer.MIN_VALUE;
+    static boolean[][] visit;
 
-	}
-	public static void dfs(int x, int y, int depth,int sum) {
-		
-		if(depth==4) {
-			max = Math.max(max, sum);
-			return;
-		}
-		
-		for(int i=0;i<4;i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(nx>=0&&ny>=0&&nx<n&&ny<m) {
-				if(!visit[nx][ny]) {
-					visit[nx][ny]=true;
-					dfs(nx,ny,depth+1,sum+map[nx][ny]);
-					visit[nx][ny]=false;
-				}
-				if(!visit[nx][ny]&&depth==2) {
-					visit[nx][ny]=true;
-					dfs(x,y,depth+1,sum+map[nx][ny]);
-					visit[nx][ny]=false;
-				}
-			}
-			
-		}
-		
-		
-	}
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0 , -1, 1};
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visit = new boolean[n][m];
+
+        for(int i = 0; i < map.length; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < map[i].length; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[i].length; j++){
+                visit[i][j] = true;
+                sol(1, i, j, map[i][j]);
+                visit[i][j] = false;
+            }
+        }
+
+        System.out.println(result);
+    }
+    public static void sol(int depth, int x, int y, int sum){
+        if(depth == 4){
+            result = Math.max(result, sum);
+            return;
+        }
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length){
+                if(!visit[nx][ny]){
+                    if(depth == 2){
+                        visit[nx][ny] = true;
+                        sol(depth + 1, x, y, sum + map[nx][ny]);
+                        visit[nx][ny] = false;
+                    }
+                    visit[nx][ny] = true;
+                    sol(depth + 1, nx, ny, sum + map[nx][ny]);
+                    visit[nx][ny] = false;
+
+                }
+            }
+
+        }
+    }
 }
