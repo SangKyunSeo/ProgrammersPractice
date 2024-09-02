1452641ed7bb7dfe6ca7d99b0a4b54586f8f514e
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-09-02 21:41:50 +0900

    [Silver II] Title: 로또, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,55 +1,59 @@
-
-/*
- * 로또 - 브루트포스
+/**
+ * Subject : BruteForce
+ * Problem Number : 6603
+ * Title : 로또
+ * Solution Trial Date : 2024-09-02
  */
-import java.util.*;
-import java.io.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int k = 0;
-	static List<Integer> list;
-	static boolean[] visit;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		while(true){
-			list = new ArrayList<>();
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			
-			if(k==0)break;
-			
-			visit = new boolean[k];
-			
-			for(int i=0;i<k;i++) {
-				list.add(Integer.parseInt(st.nextToken()));
-			}
-			
-			dfs(0,0);
-			System.out.println();
-		}
-		
-	}
-	public static void dfs(int depth, int start) {
-		
-		if(depth==6) {
-			sb = new StringBuilder();
-			for(int i=0;i<k;i++) {
-				if(visit[i]) {
-					sb.append(list.get(i));
-					sb.append(" ");
-				}
-			}
-			System.out.println(sb.toString());
-		}
-		
-		for(int i=start;i<k;i++) {
-			visit[i]=true;
-			dfs(depth+1,i+1);
-			visit[i]=false;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(st.nextToken());
 
+
+            if(size == 0) {
+                System.out.println(sb);
+                return;
+            }
+
+            int[] numbers = new int[size];
+            int[] result = new int[6];
+            for(int i = 0; i < numbers.length; i++){
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
+
+            sol(0,0, 6, numbers, result, sb);
+            sb.append("\n");
+        }
+    }
+
+    public static void sol(int depth, int idx, int size, int[] numbers, int[] result, StringBuilder sb){
+        if(depth == size){
+            printResult(result, sb);
+            return;
+        }
+
+        for(int i = idx; i < numbers.length; i++){
+            result[depth] = numbers[i];
+            sol(depth + 1, i + 1, size, numbers, result, sb);
+        }
+    }
+
+    public static void printResult(int[] result, StringBuilder sb){
+        for(int i = 0; i < result.length; i++){
+            sb.append(result[i]);
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("\n");
+    }
 }
