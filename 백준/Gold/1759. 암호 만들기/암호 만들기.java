e94ce60d1848170e05ea3b0df6ea44f0681f33f0
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-09-08 18:23:37 +0900

    [Gold V] Title: 암호 만들기, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,10 +1,3 @@
-/**
- * Subject : BruteForce
- * Problem Number : 1759
- * Title : 암호 만들기
- * Solution Trial Date : 2024-08-25
- */
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,9 +6,11 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     static char[] map;
-    static char[] result;
     static StringBuilder sb = new StringBuilder();
+
+    static char[] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,12 +18,14 @@ public class Main {
 
         int l = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
+
         map = new char[c];
         result = new char[l];
+
         st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < map.length; i++){
-            map[i] = st.nextToken().toString().charAt(0);
+            map[i] = st.nextToken().charAt(0);
         }
 
         Arrays.sort(map);
@@ -37,54 +34,34 @@ public class Main {
         System.out.println(sb);
     }
 
-    public static void sol(int idx, int depth, int end){
+    public static void sol(int depth,int idx, int end){
         if(depth == end){
-            // 한 개 이상 모음 두 개 이상 자음 검사
-            if(check()){
-                // 조건 통과 시 print
-                printResult();
-                return;
-            }
+            if(checkValidate()) printResult();
             return;
         }
 
         for(int i = idx; i < map.length; i++){
             result[depth] = map[i];
-            sol(i+1, depth + 1, end);
+            sol(depth+1, i+1, end);
         }
+
+
     }
 
-    public static boolean check(){
-        int[] count = checkCount();
-        if(count[0] >= 1 && count[1] >= 2) return true;
-        return false;
-    }
-
-    public static int[] checkCount(){
-        int[] count = new int[2];
+    public static boolean checkValidate(){
+        int mo = 0;
+        int za = 0;
         for(char c : result){
-            switch(c){
-                case 'a':
-                    count[0]++;
-                    break;
-                case 'e':
-                    count[0]++;
-                    break;
-                case 'i':
-                    count[0]++;
-                    break;
-                case 'o':
-                    count[0]++;
-                    break;
-                case 'u':
-                    count[0]++;
-                    break;
-                default:
-                    count[1]++;
-                    break;
-            }
+            if(c == 'a') mo++;
+            else if(c == 'e') mo++;
+            else if(c == 'i') mo++;
+            else if(c == 'o') mo++;
+            else if(c == 'u') mo++;
+            else za++;
         }
-        return count;
+
+        if(mo >= 1 && za >= 2) return true;
+        return false;
     }
 
     public static void printResult(){
@@ -93,4 +70,6 @@ public class Main {
         }
         sb.append("\n");
     }
+
+
 }
