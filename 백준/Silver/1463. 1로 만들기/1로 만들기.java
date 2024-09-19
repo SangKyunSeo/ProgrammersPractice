eedb41a62b004c6f69aa7d97e311593a0f3cd32d
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2024-09-19 23:24:09 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,31 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int n;
-	static int[] dp;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
-		dp[0]=0;
-		dp[1]=0;
-		for(int i=2;i<=n;i++) {
-			if(i%6==0)dp[i] = Math.min(dp[i-1],Math.min(dp[i/2], dp[i/3]))+1;
-			else if(i%2==0)dp[i] = Math.min(dp[i-1], dp[i/2])+1;
-			else if(i%3==0)dp[i] = Math.min(dp[i-1], dp[i/3])+1;
-			else dp[i]=dp[i-1]+1;
-		}
-		System.out.println(dp[n]);
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int dp[] = new int[n + 1];
+
+        if(n == 1) System.out.println(0);
+        else if(n <= 3) System.out.println(1);
+        else{
+            dp[1] = 0;
+            dp[2] = 1;
+            dp[3] = 1;
+
+            for(int i = 4; i <= n; i++){
+                if(i % 6 == 0) dp[i] = Math.min(dp[i - 1] + 1, Math.min(dp[i / 2] + 1, dp[i / 3] + 1));
+                else if(i % 2 == 0) dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
+                else if(i % 3 == 0) dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
+                else dp[i] = dp[i - 1] + 1;
+            }
+
+            System.out.println(dp[n]);
+        }
+
+
+    }
 }
