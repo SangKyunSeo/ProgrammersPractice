df74316406f6c4c285fd9af0b3814fc6e7b14475
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2025-01-06 22:48:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,34 +2,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int height[] = new int[9];
-        int sum = 0;
-        for(int i = 0; i < 9; i++){
-            height[i] = Integer.parseInt(br.readLine());
-            sum += height[i];
-        }
-
-        for(int i = 0; i < height.length - 1; i++){
-            sum -= height[i];
-            for(int j = i+1; j < height.length; j++){
-                sum -= height[j];
-                if(sum == 100){
-                    height[i] = -1;
-                    height[j] = -1;
-                    Arrays.sort(height);
-                    for(int k = 2; k < height.length; k++){
-                        System.out.println(height[k]);
-                    }
-                    return;
-                }
-                sum += height[j];
-            }
-            sum += height[i];
-        }
+  static List<Integer> result = new ArrayList<>();
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nums[] = new int[9];
+    int totalSum = 0;
+    for(int i = 0; i < 9 ; i++){
+      nums[i] = Integer.parseInt(br.readLine());
+      totalSum += nums[i];
     }
+
+    find(0, 0, 2, nums, totalSum);
+    Collections.sort(result);
+    for(int i : result){
+      System.out.println(i);
+    }
+    
+  }
+
+  public static void find(int idx, int cnt, int end, int nums[], int totalSum){
+    if(cnt == end){
+      if(totalSum == 100){
+        for(int i = 0 ; i < nums.length; i++){
+          if(nums[i] != 0) result.add(nums[i]);
+        }
+      }
+      return;
+    }
+
+    for(int i = idx; i < 9; i++){
+      if(result.size() == 7) return;
+
+      int temp = nums[i];
+      totalSum -= temp;
+      nums[i] = 0;
+      find(i + 1, cnt + 1, end, nums, totalSum);
+
+      nums[i] = temp; 
+      totalSum += temp;
+    }
+
+  }
 }
