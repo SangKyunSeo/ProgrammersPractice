49b70b8257f096d4f120cb61b87e5dbf1c377430
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2025-01-08 01:17:41 +0900

    [Silver II] Title: 사탕 게임, Time: 116 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -4,90 +4,79 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char map[][] = new char[n][n];
-        int result = -1;
-        // input 값 배열 세팅
-        for(int i = 0; i < map.length; i++){
-            String input = br.readLine();
-            for(int j = 0; j < map[0].length; j++){
-                map[i][j] = input.charAt(j);
-            }
-        }
-
-        // 교환전 result max값 구하기
-        for(int i = 0; i < map.length; i++){
-            result = Math.max(result, colMax(i, map.length, map));
-            result = Math.max(result, rowMax(i, map.length, map));
-        }
-        // 로직
-        for(int i = 0; i < map.length; i++){
-            for(int j = 0; j < map[i].length; j++){
-                int col = j + 1;
-                int row = i + 1;
-
-                // 왼쪽 열 변경
-                if(col < map.length){
-                    char temp = map[i][j];
-                    map[i][j] = map[i][col];
-                    map[i][col] = temp;
-
-                    // result max값 구하기
-                    result = Math.max(result, colMax(i, map.length, map)); // 열에 해당하는 result Max
-                    result = Math.max(result,Math.max(rowMax(j, map.length, map), rowMax(col, map.length, map))); // 바뀐 열에 따른 행에 해당하는 result Max
-                    map[i][col] = map[i][j];
-                    map[i][j] = temp;
-                }
-
-                // 아래쪽 행 변경
-                if(row < map[i].length){
-                    char temp = map[i][j];
-                    map[i][j] = map[row][j];
-                    map[row][j] = temp;
-
-                    // result max값 구하기
-                    result = Math.max(result, rowMax(j, map.length, map));
-                    result = Math.max(result, Math.max(colMax(i, map[i].length, map), colMax(row, map[i].length, map)));
-                    map[row][j] = map[i][j];
-                    map[i][j] = temp;
-                }
-            }
-        }
-
-        System.out.println(result);
+  static int result = 0;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    char map[][] = new char[n][n];
+    for(int i = 0; i < n; i++){
+      char inputArr[] = br.readLine().toCharArray();
+      for(int j = 0; j < inputArr.length; j++){
+        map[i][j] = inputArr[j];
+      }
     }
 
-    public static int rowMax(int col, int n, char[][] map){
-        char start = map[0][col];
-        int cnt = 1;
-        int max = 1;
-        for(int i = 1; i < n; i++){
-            if(start == map[i][col]){
-                cnt++;
-            }else{
-                if(max < cnt) max = cnt;
-                start = map[i][col];
-                cnt = 1;
-            }
-        }
-        return Math.max(max, cnt);
+    for(int i = 0; i < n; i++){
+      calMaxCol(map, i);
+      calMaxRow(map, i);
     }
 
-    public static int colMax(int row, int n, char[][] map){
-        char start = map[row][0];
-        int cnt = 1;
-        int max = 1;
-        for(int i = 1; i < n; i++){
-            if(start == map[row][i]){
-                cnt++;
-            }else{
-                if(max < cnt) max = cnt;
-                start = map[row][i];
-                cnt = 1;
-            }
+    for(int i = 0; i < n; i++ ){
+      for(int j = 0; j < n; j++){
+        char temp = map[i][j];
+        // 행 바꿈
+        if(i + 1 < n){
+          map[i][j] = map[i+1][j];
+          map[i+1][j] = temp;
+          calMaxCol(map, i);
+          calMaxCol(map, i+1);
+          calMaxRow(map, j);
+          map[i+1][j] = map[i][j];
+          map[i][j] = temp;
         }
-        return Math.max(max, cnt);
+
+        // 열 바꿈
+        if(j + 1 < n){
+          map[i][j] = map[i][j+1];
+          map[i][j+1] = temp;
+          calMaxRow(map, j);
+          calMaxRow(map, j+1);
+          calMaxCol(map, i);
+          map[i][j+1] = map[i][j];
+          map[i][j] = temp;
+        }
+      }
     }
+
+    System.out.println(result);
+  }
+
+  public static void calMaxCol(char[][] map, int row){
+    char start = map[row][0];
+    int sum = 1;
+    for(int i = 1; i < map.length; i++){
+      if(start == map[row][i]) sum += 1;
+      else{
+        result = Math.max(result, sum);
+        sum = 1;
+        start = map[row][i];
+      }
+    }
+    result = Math.max(result, sum);
+  }
+
+  public static void calMaxRow(char[][] map, int col){
+    char start = map[0][col];
+    int sum = 1;
+    for(int i = 1; i < map[0].length; i++){
+      if(start == map[i][col]) sum += 1;
+      else{
+        result = Math.max(result, sum);
+        sum = 1;
+        start = map[i][col];
+      }
+    }
+    result = Math.max(result, sum);
+  }
+
 }
