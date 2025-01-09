012467c9731069eddf7827e709a1d198a9adb640
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2025-01-09 23:09:48 +0900

    [Silver V] Title: 날짜 계산, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,28 +1,31 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] result = new int[3];
-        for(int i = 0; i < result.length; i++){
-            result[i] = Integer.parseInt(st.nextToken());
-        }
-        int e = 1;
-        int m = 1;
-        int s = 1;
-        int cnt = 1;
-        while(e != result[0] || s != result[1] || m != result[2]){
-            e = (e + 1) % 16 == 0 ? 1 : (e + 1) % 16;
-            s = (s + 1) % 29 == 0 ? 1 : (s + 1) % 29;
-            m = (m + 1) % 20 == 0 ? 1 : (m + 1) % 20;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-            cnt++;
-        }
-        System.out.println(cnt);
+    int e = Integer.parseInt(st.nextToken());
+    int s = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
 
+    int es = 1;
+    int ss = 1;
+    int ms = 1;
+
+    int cnt = 0;
+    while(e != es || s != ss || m != ms){
+      cnt++;
+
+      es = (es + 1) % 16 == 0 ? 1 : (es + 1) % 16;
+      ss = (ss + 1) % 29 == 0 ? 1 : (ss + 1) % 29;
+      ms = (ms + 1) % 20 == 0 ? 1 : (ms + 1) % 20;
     }
+    System.out.println(cnt + 1);
+  }
+  
 }
