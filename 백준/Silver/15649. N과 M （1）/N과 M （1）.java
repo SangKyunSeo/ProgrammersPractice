1a806652293f5841c80c0bfe782867cfaf32efc0
SangKyun-Seo <105791913+SangKyunSeo@users.noreply.github.com>
2026-02-10 19:52:01 +0900

    [Silver III] Title: N과 M (1), Time: 660 ms, Memory: 49080 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,72 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean visit[];
-    static StringBuilder sb;
+
+    private static int[] map = null;
+    private static boolean[] visit = null;
+    private static int[] result = null;
+    private static int index = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 입력부
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        visit = new boolean[n + 1];
+        map = new int[n];
+        visit = new boolean[n];
+        result = new int[m];
 
         for(int i = 1; i <= n; i++){
-            Arrays.fill(visit, false);
-            sb = new StringBuilder();
+            map[i-1] = i;
+        }
+
+        solution(n, m);
+    }
+
+    private static void solution(int n, int m){
+        for(int i = 0; i < n; i++){
             visit[i] = true;
-            sb.append(i).append(" ");
-            solution(1, m);
+            result[index] = map[i];
+            index += 1;
+            dfs(n, 1, m);
+            visit[i] = false;
+            index = 0;
         }
     }
-    public static void solution(int count, int end){
-        if(count == end){
-           System.out.println(sb.toString().trim());
+
+    private static void dfs(int n, int depth, int end){
+        if(depth == end){
+            printResult();
             return;
         }
 
-        for(int i = 1; i < visit.length; i++){
+        for(int i = 0; i < n; i++){
             if(!visit[i]){
                 visit[i] = true;
-                sb.append(i).append(" ");
-                solution(count+1, end);
+                result[index] = map[i];
+                index += 1;
+                dfs(n, depth + 1, end);
                 visit[i] = false;
-                sb.delete(sb.length() - 2, sb.length());
+                index -= 1;
+
             }
         }
+    }
+
+    private static void printResult() {
+        StringBuilder sb = new StringBuilder();
+        for(int i : result){
+            sb.append(i);
+            sb.append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
     }
 }
